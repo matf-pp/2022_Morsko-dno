ab84126daae087756083a4982ad242f31eaa8e70
NevenaM12 <101059677+NevenaM12@users.noreply.github.com>
2022-05-04 19:12:25 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
   runApp(MyApp());
-}
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
 
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitleBarStyle(TitleBarStyle.normal);
+    await windowManager.maximize();
+    await windowManager.setMinimumSize;
+    await windowManager.setMinimizable(true);
+    await windowManager.show();
+    await windowManager.setSkipTaskbar(false);
+  });
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
