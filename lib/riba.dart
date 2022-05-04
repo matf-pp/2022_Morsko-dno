52a06574ec787e469d4e7fdded84195ad19ad91d
MarijaBB <101042225+MarijaBB@users.noreply.github.com>
2022-05-04 17:22:10 +0200

    Update riba.dart

--- lib/riba.dart
+++ lib/riba.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
 class MyRiba extends StatelessWidget {
+  final indr;
+
+  MyRiba({this.indr});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset(
+    if (indr==1) {
+      return Container(
+        child: Image.asset(
           'lib/slike/riba.png',
-        height:80,
-        width:80,
-      ),
-    );
+          height: 80,
+          width: 80,
+        ),
+      );
+    }else if(indr==2) {
+      return Container(
+        child: Image.asset(
+          'lib/slike/riba2.png',
+          height: 90,
+          width: 90,
+        ),
+      );
+    }else if(indr==3){
+      return Container(
+        //alignment: Alignment.center,
+        child: Image.asset(
+          'lib/slike/riba3.png',
+          height:80,
+          width:80,
+        ),
+      );
+    } else{
+      return Container(
+        child: Image.asset(
+          'lib/slike/riba4.png',
+          height: 100,
+          width: 100,
+        ),
+      );
+    }
   }
 }
